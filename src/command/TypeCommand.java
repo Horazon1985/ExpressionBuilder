1c51c06e8c0dab65296f282e106e437204bda419
kovalens <kovalens@MSGN01630.int.root.msg.ag>
2015-12-15 10:08:20 +0100

    Update - Befehlstyp für das Lösen linearer Gleichungssysteme implementiert + entsprechende Fehlermeldungen (auf DE und EN) in die Sprach-XMLs eingefügt.

--- src/command/TypeCommand.java
+++ src/command/TypeCommand.java
@@ -4,6 +4,6 @@ public enum TypeCommand {
 
     approx, ccnf, cdnf, clear, def, deffuncs, defvars, eigenvalues, eigenvectors, 
     euler, expand, ker, latex, pi, plot2d, plotimplicit, plot3d, plotcurve, plotpolar, solve, 
-    solvedeq, table, tangent, taylordeq, undef, undefall;
+    solvedeq, solvesystem, table, tangent, taylordeq, undef, undefall;
 
 }
