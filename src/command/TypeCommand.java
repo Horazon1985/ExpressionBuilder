e8b72260a6c8bf0af8a14b93d9b4ba572b81e9b0
kovalens <kovalens@MSGN01630.int.root.msg.ag>
2016-02-04 08:43:12 +0100

    Update - Neue Befehle undefvar() und undeffunc() implementiert (Fehlermeldungen fehlen noch).

--- src/command/TypeCommand.java
+++ src/command/TypeCommand.java
@@ -4,6 +4,7 @@ public enum TypeCommand {
 
     approx, ccnf, cdnf, clear, def, deffuncs, defvars, eigenvalues, eigenvectors, 
     euler, expand, ker, latex, pi, plot2d, plotimplicit, plot3d, plotcurve, plotpolar, 
-    regressionline, solve, solvedeq, solvesystem, table, tangent, taylordeq, undef, undefall;
+    regressionline, solve, solvedeq, solvesystem, table, tangent, taylordeq, undeffunc, 
+    undefvar, undefall;
 
 }
