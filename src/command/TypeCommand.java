7e4aff89acc3e1aca819ce763b4d8fa639bbb56b
kovalens <kovalens@MSGN01630.int.root.msg.ag>
2016-02-10 11:05:13 +0100

    Update.

--- src/command/TypeCommand.java
+++ src/command/TypeCommand.java
@@ -2,9 +2,8 @@ package command;
 
 public enum TypeCommand {
 
-    extrema,
     approx, ccnf, cdnf, clear, def, deffuncs, defvars, eigenvalues, eigenvectors, 
-    euler, expand, ker, latex, pi, plot2d, plotimplicit, plot3d, 
+    euler, expand, extrema, ker, latex, pi, plot2d, plotimplicit, plot3d, 
     plotcurve2d, plotcurve3d, plotpolar, regressionline, solve, solvedeq, 
     solvesystem, table, tangent, taylordeq, undeffuncs, undefvars, undefallfuncs, 
     undefallvars, undefall;
