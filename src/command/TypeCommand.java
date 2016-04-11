ecac99c442d3b32cb42eaba3f883aaee90d6142a
Sergei Kovalenko <kovalenko@arcor.de>
2016-04-11 17:14:27 +0200

    Update - In TypeCommand die Konstante plotvectorfield3D temporär entfernt (weil noch in Arbeit).

--- src/command/TypeCommand.java
+++ src/command/TypeCommand.java
@@ -5,7 +5,8 @@ public enum TypeCommand {
     approx, ccnf, cdnf, clear, def, deffuncs, defvars, eigenvalues, eigenvectors, 
     euler, expand, extrema, ker, latex, pi, plot2d, plotimplicit, plot3d, 
     plotcurve2d, plotcurve3d, plotpolar, plotcylindrical, plotspherical, 
-    plotvectorfield2d, plotvectorfield3d, regressionline, 
+    plotvectorfield2d, //plotvectorfield3d, 
+    regressionline, 
     solve, solvediffeq, solvesystem, table, tangent, taylordiffeq, undeffuncs, undefvars, 
     undefallfuncs, undefallvars, undefall;
 
