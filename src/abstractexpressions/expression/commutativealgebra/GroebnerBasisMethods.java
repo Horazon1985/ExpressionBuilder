e95290c4c389e2aec7e559c75717d3f9e62ca30e
Sergei Kovalenko <kovalenko@arcor.de>
2016-04-15 18:10:14 +0200

    Update - An Gröbnerbasen weitergearbeitet.

--- src/abstractexpressions/expression/commutativealgebra/GroebnerBasisMethods.java
+++ src/abstractexpressions/expression/commutativealgebra/GroebnerBasisMethods.java
@@ -1,29 +1,42 @@
 package abstractexpressions.expression.commutativealgebra;
 
 import abstractexpressions.expression.classes.Expression;
+import static abstractexpressions.expression.classes.Expression.ZERO;
+import abstractexpressions.expression.classes.Variable;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class GroebnerBasisMethods {
 
-    public class Monomial {
+    /**
+     * Konstanten, welche Standardtermordnungen repräsentieren.
+     */
+    private enum TermOrderings {
 
-        private int dim;
+        LEX, DEGLEX, REVLEX, DEGREVLEX;
+    }
+
+    private static String[] monomialVars;
+    private static TermOrderings termOrdering;
+
+    public class Monomial implements Comparable<Monomial> {
+
         private Expression coefficient;
-        private Expression[] term;
+        private int[] term;
 
-        public Monomial(int dim, Expression coefficient, Expression[] term) {
-            this.dim = dim;
+        public Monomial(Expression coefficient, int[] term) {
             this.coefficient = coefficient;
-            this.term = term;
-        }
-
-        public int getDim() {
-            return dim;
-        }
-
-        public void setDim(int dim) {
-            this.dim = dim;
+            this.term = new int[monomialVars.length];
+            /*
+             Damit es keine Dimensionsfehler gibt: wenn term und this.term in den
+             Dimensionen nicht übereinstimmen (warum auch immer), wird nur bis
+             zur gemeinsamen Schranke kopiert und der Rest (falls notwendig) mit 
+             Nullen aufgefüllt.
+             */
+            System.arraycopy(term, 0, this.term, 0, Math.min(monomialVars.length, term.length));
+            for (int i = Math.min(monomialVars.length, term.length); i < this.term.length; i++) {
+                this.term[i] = 0;
+            }
         }
 
         public Expression getCoefficient() {
@@ -34,24 +47,182 @@ public class GroebnerBasisMethods {
             this.coefficient = coefficient;
         }
 
-        public Expression[] getTerm() {
+        public int[] getTerm() {
             return term;
         }
 
-        public void setTerm(Expression[] term) {
+        public void setTerm(int[] term) {
             this.term = term;
+        }
+
+        public int getDegree() {
+            int degree = 0;
+            for (int exponent : term) {
+                degree += exponent;
+            }
+            return degree;
+        }
+
+        public Expression toExpression(){
+            Expression monomialAsExpression = coefficient;
+            for (int i = 0; i < this.term.length; i++){
+                monomialAsExpression = monomialAsExpression.mult(Variable.create(monomialVars[i]).pow(this.term[i]));
+            }
+            return monomialAsExpression;
+        }
+        
+        public boolean isZero(){
+            return this.coefficient.equals(ZERO);
+        }
+        
+        public Monomial multiplyWithMonomial(Monomial m){
+            Expression resultCoefficient = this.coefficient.mult(m.coefficient);
+            int[] resultTerm = new int[this.term.length];
+            for (int i = 0; i < this.term.length; i++){
+                resultTerm[i] = this.term[i] + m.term[i];
+            }
+            return new Monomial(resultCoefficient, resultTerm);
+        }
+        
+        @Override
+        public int compareTo(Monomial m) {
+            if (termOrdering == TermOrderings.LEX) {
+                return compateToWithRespectToLex(m);
+            }
+            if (termOrdering == TermOrderings.DEGLEX) {
+                return compateToWithRespectToDegLex(m);
+            }
+            if (termOrdering == TermOrderings.REVLEX) {
+                return compateToWithRespectToRevLex(m);
+            }
+            if (termOrdering == TermOrderings.DEGREVLEX) {
+                return compateToWithRespectToDegRevLex(m);
+            }
+            return 0;
+        }
+
+        private int compateToWithRespectToLex(Monomial m) {
+            for (int i = 0; i < this.term.length; i++) {
+                if (this.term[i] < m.term[i]) {
+                    return -1;
+                }
+                if (this.term[i] > m.term[i]) {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+
+        private int compateToWithRespectToDegLex(Monomial m) {
+            if (this.getDegree() > m.getDegree()) {
+                return 1;
+            }
+            if (this.getDegree() < m.getDegree()) {
+                return -1;
+            }
+            return compateToWithRespectToLex(m);
+        }
+
+        private int compateToWithRespectToRevLex(Monomial m) {
+            for (int i = this.term.length - 1; i >= 0; i++) {
+                if (this.term[i] < m.term[i]) {
+                    return 1;
+                }
+                if (this.term[i] > m.term[i]) {
+                    return -1;
+                }
+            }
+            return 0;
+        }
+
+        private int compateToWithRespectToDegRevLex(Monomial m) {
+            if (this.getDegree() > m.getDegree()) {
+                return 1;
+            }
+            if (this.getDegree() < m.getDegree()) {
+                return -1;
+            }
+            return compateToWithRespectToRevLex(m);
         }
 
     }
 
     public class MultiPolynomial {
 
-        private ArrayList<Monomial> monomials = new ArrayList<>();
+        private final ArrayList<Monomial> monomials = new ArrayList<>();
+
+        public MultiPolynomial() {
+        }
         
-        public MultiPolynomial(Monomial... monomials){
+        public MultiPolynomial(ArrayList<Monomial> monomials) {
+            this.monomials.addAll(monomials);
+        }
+
+        public MultiPolynomial(Monomial... monomials) {
             this.monomials.addAll(Arrays.asList(monomials));
+        }
+
+        public ArrayList<Monomial> getMonomials() {
+            return this.monomials;
+        }
+
+        public void addMonomial(Monomial m){
+            this.monomials.add(m);
+        }
+        
+        public Expression toExpression(){
+            Expression multiPloynomialAsExpression = ZERO;
+            for (Monomial m : this.monomials){
+                multiPloynomialAsExpression = multiPloynomialAsExpression.add(m.toExpression());
+            }
+            return multiPloynomialAsExpression;
+        }
+
+        public void clearZeroMonomials(){
+            // TO DO.
+        }
+        
+        public boolean isZero(){
+            return this.monomials.isEmpty();
+        }
+        
+        public MultiPolynomial multiplyWithMonomial(Monomial m){
+            MultiPolynomial resultPolynomial = new MultiPolynomial();
+            for (Monomial monomial : this.monomials){
+                resultPolynomial.addMonomial(monomial.multiplyWithMonomial(m));
+            }
+            return resultPolynomial;
         }
         
     }
+
+    public String[] getMonomialVars() {
+        return monomialVars;
+    }
+
+    public void setMonomialVars(String[] monomialVars) {
+        GroebnerBasisMethods.monomialVars = monomialVars;
+    }
+
+    private static Monomial getMaximalMonomial(ArrayList<Monomial> monomials) {
+        Monomial maxMonomial = null;
+        for (Monomial m : monomials){
+            if (maxMonomial == null){
+                maxMonomial = m;
+            } else if (maxMonomial.compareTo(m) < 0){
+                maxMonomial = m;
+            }
+        }
+        return maxMonomial;
+    }
+
+    private static MultiPolynomial getSyzygyPolynomial(MultiPolynomial f, MultiPolynomial g){
+
+        
+        
+        return null;
+    }
+    
+    
 
 }
---
+++ test/expression/computationtests/GroebnerBasisTests.java
@@ -0,0 +1,32 @@
+package expression.computationtests;
+
+import abstractexpressions.expression.classes.Expression;
+import exceptions.EvaluationException;
+import exceptions.ExpressionException;
+import junit.framework.Assert;
+import org.junit.AfterClass;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import utilities.TestUtilities;
+
+public class GroebnerBasisTests {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void defineExpressions() throws Exception {
+    }
+
+    @Test
+    public void expandAlgebraicExpressionTest1() {
+    }
+
+}
