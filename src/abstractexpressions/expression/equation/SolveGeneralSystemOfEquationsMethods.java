f9addb7e39eefb2563331d6977bb8dd84cd0a56d
Sergei Kovalenko <kovalenko@arcor.de>
2016-04-24 22:35:39 +0200

    Update - Angefangen, polynomielle Gleichungssysteme zu implementieren.

---
+++ src/abstractexpressions/expression/equation/SolveGeneralSystemOfEquationsMethods.java
@@ -0,0 +1,28 @@
+package abstractexpressions.expression.equation;
+
+import abstractexpressions.expression.classes.Expression;
+import java.util.ArrayList;
+
+public class SolveGeneralSystemOfEquationsMethods {
+
+    private static boolean isSystemInTriangularForm(Expression[] equations, String[] vars){
+    
+    
+    
+        return true;
+        
+    }
+    
+    public static ArrayList<ArrayList<Expression>> solvePolynomialSystemOfEquations(Expression[] equations, String[] vars){
+    
+        ArrayList<ArrayList<Expression>> solutions = new ArrayList<>();
+        
+        
+        
+        return solutions;
+    
+    }
+    
+    
+    
+}
